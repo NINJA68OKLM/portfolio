3a1ff981d2277b7873ff2d8e1b77ba56d5deebdb
NINJA68OKLM <85460270+NINJA68OKLM@users.noreply.github.com>
2022-09-25 16:24:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// // Menu mobile
-// document.querySelector(".header-item").addEventListener("click", elements);
-// document.querySelector(".sub-header>svg").addEventListener("click", elements);
-// function elements(){
-//     document.querySelector(".header.mobile").classList.toggle("hide");
-// }
-
 // Portfolio V2
 document.querySelector(".partone>svg").addEventListener("click", submenu);
 function submenu(){
@@ -14,45 +7,90 @@ function submenu(){
 }
 // Menu
 if (window.matchMedia("(min-width: 1200px)").matches) {
+    
     document.querySelectorAll(".hide").forEach(e => {
         e.classList.toggle("hide");
     });
     console.log(screen.width);
     document.querySelector("main").style.width= (screen.width)-222+"px";
     var footerwidth = document.querySelector("footer").style.width;
-    // document.querySelectorAll("a, li").forEach(e => {
-    //     e.
-    // });
+    // Pied de page
+    console.log(screen.height);
+    var main = document.querySelector("main");
+    if (main.offsetWidth < screen.height) {
+        main.style.height = screen.height+"px";
+    }
+    // Menu sous-menus
+    document.querySelector(".ul-menu").classList.add("hide");
+    document.querySelector(".declencheur").addEventListener("mouseover", opacity);
+    document.querySelector(".declencheur").addEventListener("mouseout", opacity);
+    function opacity(){
+        document.querySelector(".ul-menu").classList.toggle("hide");
+    }
+}
+// A propos
+if (window.matchMedia("(min-width: 680px)").matches) {
     // Les images d'A propos
     var images = document.querySelectorAll(".img");
     images.forEach(e => {
-        e.style.height = e.getAttribute("Id")+"%";
-        // console.log(e.getAttribute("Id"));
+        e.style.height = e.getAttribute("pour-id")+"%";
+    });
+
+    // Gallerie
+    var galleryimage = document.querySelectorAll(".grid.gallery.lycee>img");
+    galleryimage.forEach(e=> {
+        e.addEventListener("click", function(){
+            var fullscreen = document.createElement("div");
+            fullscreen.classList.add("fullscreen");
+            fullscreen.style.width= document.querySelector("main").style.width;
+            fullscreen.style.height= "100vh";
+            fullscreen.innerHTML= `<svg style='margin-left: auto; margin-right: 10px;' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                <img style='margin-top: 10px;' src='${e.getAttribute("src")}' alt='${e.getAttribute("alt")}' width='90%'>`;
+            fullscreen.style.position= "fixed";
+            fullscreen.style.top= "0px";
+            fullscreen.style.right= "0px";
+            fullscreen.style.display= "flex";
+            fullscreen.style.flexDirection= "column";
+            fullscreen.style.alignItems= "center";
+            fullscreen.style.background= "#000000";
+            fullscreen.style.padding= "10px";
+            document.querySelector("body").appendChild(fullscreen);
+            // Fermer le plein écran
+            var fermer = document.querySelector(".fullscreen > svg");
+            fermer.addEventListener("click", function(){
+                fullscreen = document.querySelector(".fullscreen");
+                this.parentNode.parentNode.removeChild(fullscreen);
+            });
+        });
     });
 }
-// Réalisations et A propos
+// Réalisations et A propos et sous catégories
+var lien = window.location.href;
+var tab = lien.split("/");
 var logos = document.querySelectorAll(".blanc, .fond.deux");
 logos.forEach(e => {
     var Idd = e.parentNode.getAttribute("id");
-    e.style.backgroundImage= "url('img/logos/"+Idd+".png')";
+    if (tab.length == 10)
+    {
+        e.style.backgroundImage= "url('../img/"+Idd+"')";
+    }
+    else
+    {
+        e.style.backgroundImage= "url('img/"+Idd+"')";
+    }
     e.style.backgroundSize= "contain";
     e.style.backgroundRepeat= "no-repeat";
     e.style.backgroundPosition= "center center";
-    // console.log(Idd);
 });
+console.log(tab);
+console.log(window.location.href);
 // Compétences
 var progress = document.querySelectorAll(".progress");
 progress.forEach(e => {
     e.style.width= e.getAttribute("val-id")+"%";
-    // if (e.getAttribute("val-id") == 100) {
-    //     e.style.borderRadius= "10px";
-    // } else {
-    //     e.style.borderRadius= "10px 0px 0px 10px";
-    // }
 });
 
 // Animation 
-
 var elements = document.querySelectorAll(".bloc, h2");
 elements.forEach(e => {
     setTimeout(() => {
@@ -60,6 +98,7 @@ elements.forEach(e => {
     }, e.getAttribute("tempp-id"));
     e.style.animation= "appear 2s";
     e.style.animationDelay= e.getAttribute("temp-id")+"s";
-    // console.log(e.getAttribute("temp-id"));
-    // console.log(e.getAttribute("tempp-id"))
 });
+
+// Animation de fond
+document.querySelector(".area").style.height= document.querySelector("main").offsetHeight+"px";
